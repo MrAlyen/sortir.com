41473fa5b4ede5bdcd096fa9f729507a64eea32b
PavieSylvain <pavie.sylvain@sfr.fr>
2021-09-21 14:01:11 +0200

    csv save

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,11 +4,15 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Form\ImportCsvFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
 
 class RegistrationController extends AbstractController
 {
@@ -40,6 +44,35 @@ class RegistrationController extends AbstractController
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/ajoutUtilisateur", name="app_admin_ajout_utilisateur")
+     */
+    public function ajoutUtilisateur(Request $request){
+        $form = $this->createForm(ImportCsvFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $file = $form->get('file')->getData();
+            $fileExtension = pathinfo($file->getClientOriginalName(), PATHINFO_EXTENSION);
+            $normalizer = [new ObjectNormalizer];
+            $encoder = [
+                new CsvEncoder()
+            ];
+
+            $serializer = new Serializer($normalizer, $encoder);
+            
+            $fileString = file_get_contents($file);
+            $data = $serializer->decode($fileString, $fileExtension);
+
+            dd($data);
+        }
+        
+        
+        return $this->render('admin/ajoutUtilisateur.html.twig', [
+            'csvForm' => $form->createView(),
         ]);
     }
 }
