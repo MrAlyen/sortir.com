f74266c93d94128ac2f6690cf0e0fe5b0b6618bb
Alyen <t.royer@live.fr>
2021-09-21 13:49:55 +0200

    Maj mailer

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -158,9 +158,9 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('botadmin@sortir.com', 'botadmin'))
+            ->from(new Address('testsortir.com@outlook.fr', 'botadmin'))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('demande de reinitialisation de votre mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
