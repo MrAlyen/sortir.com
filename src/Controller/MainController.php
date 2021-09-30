e748351e5886c0de0e6ab2eee03948140c3cb677
Alyen <t.royer@live.fr>
2021-09-30 18:05:12 +0200

    mon profil (manque l'image)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 
+use App\Form\ModifierProfilType;
 use App\Form\SortieFilterType;
 use App\Form\SortieType;
 use App\Repository\SortieRepository;
 use App\Utilities\Tools;
+use ContainerX2QsL9A\getModifierProfilTypeService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Sortie;
 
@@ -46,6 +49,39 @@ class MainController extends AbstractController
             'filtre' => $filtreForm->createView(),
             'sorties' => $tabSorties
         ]);
+    }
+    /**
+     * @Route("/sortir/profil", name="main_modifierProfil")
+     */
+    public function ModifierProfil(Request $request, UserPasswordHasherInterface $passwordHasher)
+    {
+        $user = $this->getUser();
+        $profil = $this->createForm(ModifierProfilType::class, $user);
+
+        $profil->handleRequest($request);
+        if ($profil->isSubmitted() && $profil->isValid()){
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            if($request->isMethod('POST')){
+                $entityManager = $this->getDoctrine()->getManager();
+                $user = $this->getUser();
+
+                // verification de le correspondance des deux mot de passe
+                if ($request->request->get('passe') == $request->request->get('passe2')){
+                    $user->setPassword($passwordHasher->hashPassword($user, $request->request->get('passe2')));
+                    $entityManager->flush();
+                    $this->addFlash('message','Votre profil est bien modifier');
+                    return $this->redirectToRoute('main_modifierProfil');
+                }else{
+                    $this->addFlash('erreur', 'Les deux mot de passe ne correspondent pas!');
+                }
+            }
+        }
+
+        return $this->render('main/modifierProfil.html.twig',['profil' => $profil->createView()]);
     }
 
 }
